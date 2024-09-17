5829f30e566305d156d554fad92c8b2af186e82b
leandrucarvalho <leandrucs@gmail.com>
2024-09-17 15:28:05 -0300

    feat: create render div, generate products and fetch products to json

--- main.ts
+++ main.ts
@@ -21,4 +21,60 @@ class Product {
     this._category = description;
     this._imageUrl = image;
   }
+
+  render(): string {
+    return `
+    <div class="container">
+      <div class="img-food">
+        <img src="${this._imageUrl}" alt="${this._category}" />
+        <button class="btn-cart">
+          <i class="fa-solid fa-cart-plus"></i> Add to Cart
+        </button>
+      </div>
+      <div class="info-food">
+        <span class="category">${this._category}</span>
+        <span class="name">${this._name}</span>
+        <span class="price">${this._price}</span>
+      </div>
+    </div>
+    `;
+  }
 }
+
+function generateProducts(data: any[]): void {
+  const productList = document.querySelector(".product-list");
+
+  data.forEach((data, index) => {
+    const product = new Product(
+      data.id,
+      data.name,
+      data.price,
+      data.quantity,
+      data.category,
+      data.image.desktop
+    );
+
+    if (productList) {
+      productList.innerHTML += `
+      <div class="product-list" id="product-${index}">
+        ${product.render()}
+      </div>
+      `;
+    }
+  });
+}
+
+async function fetchProducts() {
+  try {
+    const response = await fetch("data.json");
+    if (!response.ok) {
+      throw new Error("Falha ao carregar o arquivo JSON");
+    }
+    const data = await response.json();
+    generateProducts(data);
+  } catch (error) {
+    console.error("Ocorreu um erro ao carregar os produtos:", error);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", fetchProducts);
