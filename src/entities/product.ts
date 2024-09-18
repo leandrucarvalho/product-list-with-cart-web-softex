e3dbc1c341a7257b1621b6e4d4dba6f95a75f40a
Leandro Carvalho <leandrucs@gmail.com>
2024-09-18 10:53:39 -0300

    feat: create entitie Product

---
+++ src/entities/product.ts
@@ -0,0 +1,34 @@
+import { v4 as uuidv4 } from "uuid";
+
+export class Product {
+  private _id: string = uuidv4();
+  private _name: string;
+  private _price: number;
+  private _category: string;
+  private _imageUrl: string;
+
+  constructor(name: string, price: number, category: string, image: string) {
+    this._name = name;
+    this._price = price;
+    this._category = category;
+    this._imageUrl = image;
+  }
+
+  render(): string {
+    return `
+    <div class="container">
+      <div class="img-food">
+        <img src="${this._imageUrl}" alt="${this._category}" />
+        <button class="btn-cart">
+          <i class="fa-solid fa-cart-plus"></i> Add to Cart
+        </button>
+      </div>
+      <div class="info-food">
+        <span class="category">${this._category}</span>
+        <span class="name">${this._name}</span>
+        <span class="price">${this._price}</span>
+      </div>
+    </div>
+    `;
+  }
+}
