a90d27f802379436fde25ccadb38710eea739492
Leandro Carvalho <leandrucs@gmail.com>
2024-09-23 12:03:03 -0300

    remove: remove commits

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 import { Product } from "./src/entities/product";
 import data from "./data.json";
-import { Cart } from "./src/entities/cart";
 
-/* function generateProducts(data: any[]): void {
+function generateProducts(data: any[]): void {
   const productList = document.querySelector(".product-list");
 
   data.forEach((data) => {
@@ -19,23 +18,4 @@ import { Cart } from "./src/entities/cart";
   });
 }
 
-generateProducts(data); */
-
-const product1 = new Product(
-  data[0].name,
-  data[0].price,
-  data[0].category,
-  data[0].image.desktop
-);
-
-const product2 = new Product(
-  data[1].name,
-  data[1].price,
-  data[1].category,
-  data[1].image.desktop
-);
-
-const cart = new Cart();
-cart.addProduct(product1);
-cart.addProduct(product2);
-console.log(cart.totalPrice);
+generateProducts(data);
