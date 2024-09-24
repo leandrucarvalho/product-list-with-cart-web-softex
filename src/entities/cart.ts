741b611e9c9a8659a7bce76524863a64d355b14e
leandrucarvalho <leandrucs@gmail.com>
2024-09-24 16:37:35 -0300

    feat: add function   addProduct  which checks if a product already exists

--- src/entities/cart.ts
+++ src/entities/cart.ts
@@ -5,9 +5,21 @@ export class Cart {
   private _totalPrice: number = 0;
 
   addProduct(product: Product): void {
+    // Verificar se o produto já existe no carrinho
+    const existingProduct = this._products.find((p) => p.id === product.id);
+
+    if (existingProduct) {
+      // Se o produto já existir, atualizar a quantidade
+      existingProduct.quantity += 1;
+    } else {
+      // Se o produto não existir, adicionar ao carrinho e atualiza a qtd para 1
+      product.quantity = 1;
+      this._products.push(product);
+    }
+
     this._products.push(product);
+
     this._totalPrice += product.price;
-    console.log(this._products);
   }
 
   get products() {
