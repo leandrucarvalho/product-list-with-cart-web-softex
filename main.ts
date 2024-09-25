6141f61fb3721bab397c180ec3485d66ed0f1659
Leandro Carvalho <leandrucs@gmail.com>
2024-09-25 11:56:04 -0300

    test: testing methods

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { Product } from "./src/entities/product";
 import data from "./data.json";
+import { Cart } from "./src/entities/cart";
 
 function generateProducts(data: any[]): void {
   const productList = document.querySelector(".product-list");
@@ -19,3 +20,15 @@ function generateProducts(data: any[]): void {
 }
 
 generateProducts(data);
+
+const banana = new Product(
+  data[0].name,
+  data[0].price,
+  data[0].category,
+  data[0].image.desktop
+);
+banana.incrementQuantity();
+banana.incrementQuantity();
+
+// console.log(banana);
+console.log(Cart.products);
