e43785e7d31f423867a6ea5ae4a559adbd597235
Leandro Carvalho <leandrucs@gmail.com>
2024-09-25 11:55:28 -0300

    feat: create static method

--- src/entities/cart.ts
+++ src/entities/cart.ts
@@ -1,12 +1,13 @@
 import { Product } from "./product";
 
 export class Cart {
-  private _products: Product[] = [];
-  private _totalPrice: number = 0;
+  private static _products: Product[] = [];
+  private static _totalPriceCart: number = 0;
 
-  addProduct(product: Product): void {
+  static addProduct(product: Product): void {
     // Verificar se o produto já existe no carrinho
     const existingProduct = this._products.find((p) => p.id === product.id);
+    console.log(existingProduct);
 
     if (existingProduct) {
       // Se o produto já existir, atualizar a quantidade
@@ -17,16 +18,14 @@ export class Cart {
       this._products.push(product);
     }
 
-    this._products.push(product);
-
-    this._totalPrice += product.price;
+    this._totalPriceCart += product.totalPrice;
   }
 
-  get products() {
+  static get products() {
     return this._products;
   }
 
-  get totalPrice() {
-    return this._totalPrice;
+  static get totalPrice() {
+    return this._totalPriceCart;
   }
 }
